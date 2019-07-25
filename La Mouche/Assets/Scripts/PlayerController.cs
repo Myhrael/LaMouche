d7613b5a8f1a0edacb8d2ddbe583afb1e292b8db
Gabriel Xavier Luisier <gabriel.luisier@epfl.ch>
2019-07-25 13:30:34 +0200

    First scripts
    Add movement and import station mesh

---
+++ La Mouche/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 1;
+    public float maxAngleSpeed = 2;
+    public float angleSpeedGain = 1;
+    public Vector3 vForward = new Vector3(0, 0, 1);
+
+    public float angleSpeed = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float dt = Time.deltaTime;
+
+        float yaw = Input.GetAxis("Horizontal");
+        float pitch = Input.GetAxis("Vertical");
+        float roll = Input.GetAxis("Roll");
+
+        if((yaw!=0 || pitch!=0 || roll != 0) && angleSpeed<maxAngleSpeed)
+        {
+            angleSpeed += angleSpeedGain * dt;
+            if (angleSpeed > maxAngleSpeed) angleSpeed = maxAngleSpeed;
+        }else if (angleSpeed > 1)
+        {
+            angleSpeed -= angleSpeedGain * dt;
+            if (angleSpeed < 1) angleSpeed = 1;
+        }
+
+        transform.Rotate(pitch*angleSpeed, yaw*angleSpeed, roll*angleSpeed);
+        transform.Translate(vForward*dt);
+
+        //transform.Translate(transform.rotation * dt * speed);
+    }
+}
---
+++ La Mouche/Assets/Scripts/landingSpot.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class landingSpot : MonoBehaviour
+{
+    public Transform rayOrigin;
+    public Camera camera;
+
+    public GameObject landObject;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        Ray ray = camera.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0));
+        if(Physics.Raycast(ray, out hit, 2))
+        {
+            landObject = hit.collider.gameObject;
+        }
+        else
+        {
+            landObject = null;
+        }
+    }
+}

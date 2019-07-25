62abbcddd5463bb55dfe64f3c659e41af8fb8eef
Gabriel Xavier Luisier <gabriel.luisier@epfl.ch>
2019-07-25 14:56:31 +0200

    Made script to auto add collider

--- La Mouche/Assets/Scripts/ColliderCreator.cs
+++ La Mouche/Assets/Scripts/ColliderCreator.cs
@@ -7,7 +7,13 @@ public class ColliderCreator : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        List<GameObject> childs = new List<GameObject>();
+        getChilds(transform, childs);
+
+        foreach(GameObject obj in childs)
+        {
+            MeshCollider collider = obj.AddComponent<MeshCollider>() as MeshCollider;
+        }
     }
 
     // Update is called once per frame
@@ -16,8 +22,14 @@ public class ColliderCreator : MonoBehaviour
         
     }
 
-    private List<GameObject> addChilds(GameObject parent, List<GameObject> childs)
+    private List<GameObject> getChilds(Transform parent, List<GameObject> childs)
     {
+        foreach(Transform child in parent)
+        {
+            childs.Add(child.gameObject);
+            getChilds(child, childs);
+        }
 
+        return childs;
     }
 }
